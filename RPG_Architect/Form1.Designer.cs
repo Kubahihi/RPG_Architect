41bb08c0e2283f45f6709363b48a70ff228f7791
jakub <jakub@NTBHESJ>
2025-03-21 20:14:44 +0100

    Přidejte soubory projektu.

---
+++ RPG_Architect/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace RPG_Architect
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonAddCharacter = new Button();
+            textBoxName = new TextBox();
+            listBoxCharacters = new ListBox();
+            numericUpDownAge = new NumericUpDown();
+            numericUpDownStrength = new NumericUpDown();
+            numericUpDownHealth = new NumericUpDown();
+            richTextBox1 = new RichTextBox();
+            richTextBox2 = new RichTextBox();
+            richTextBox3 = new RichTextBox();
+            richTextBox4 = new RichTextBox();
+            button1 = new Button();
+            buttonDelete = new Button();
+            richTextBoxDetails = new RichTextBox();
+            richTextBox5 = new RichTextBox();
+            richTextBoxLore = new RichTextBox();
+            buttonExport = new Button();
+            buttonImport = new Button();
+            richTextBox6 = new RichTextBox();
+            comboBoxAdditionalAttributes = new ComboBox();
+            buttonAddAttribute = new Button();
+            textBoxAttributeValue = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownAge).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownStrength).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownHealth).BeginInit();
+            SuspendLayout();
+            // 
+            // buttonAddCharacter
+            // 
+            buttonAddCharacter.ForeColor = SystemColors.ControlText;
+            buttonAddCharacter.Location = new Point(381, 7);
+            buttonAddCharacter.Name = "buttonAddCharacter";
+            buttonAddCharacter.Size = new Size(148, 37);
+            buttonAddCharacter.TabIndex = 0;
+            buttonAddCharacter.Text = "Přidat postavu";
+            buttonAddCharacter.UseVisualStyleBackColor = true;
+            buttonAddCharacter.Click += button1_Click;
+            // 
+            // textBoxName
+            // 
+            textBoxName.Location = new Point(149, 74);
+            textBoxName.Name = "textBoxName";
+            textBoxName.Size = new Size(207, 27);
+            textBoxName.TabIndex = 1;
+            textBoxName.TextChanged += textBox1_TextChanged_1;
+            // 
+            // listBoxCharacters
+            // 
+            listBoxCharacters.FormattingEnabled = true;
+            listBoxCharacters.Location = new Point(926, 42);
+            listBoxCharacters.Name = "listBoxCharacters";
+            listBoxCharacters.Size = new Size(228, 644);
+            listBoxCharacters.TabIndex = 2;
+            listBoxCharacters.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // numericUpDownAge
+            // 
+            numericUpDownAge.Location = new Point(58, 41);
+            numericUpDownAge.Name = "numericUpDownAge";
+            numericUpDownAge.Size = new Size(64, 27);
+            numericUpDownAge.TabIndex = 3;
+            // 
+            // numericUpDownStrength
+            // 
+            numericUpDownStrength.Location = new Point(58, 74);
+            numericUpDownStrength.Name = "numericUpDownStrength";
+            numericUpDownStrength.Size = new Size(64, 27);
+            numericUpDownStrength.TabIndex = 4;
+            // 
+            // numericUpDownHealth
+            // 
+            numericUpDownHealth.Location = new Point(58, 107);
+            numericUpDownHealth.Name = "numericUpDownHealth";
+            numericUpDownHealth.Size = new Size(64, 27);
+            numericUpDownHealth.TabIndex = 5;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(-1, 40);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(53, 27);
+            richTextBox1.TabIndex = 6;
+            richTextBox1.Text = "Věk";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(-1, 73);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(53, 27);
+            richTextBox2.TabIndex = 7;
+            richTextBox2.Text = "Síla";
+            richTextBox2.TextChanged += richTextBox2_TextChanged;
+            // 
+            // richTextBox3
+            // 
+            richTextBox3.Location = new Point(-1, 106);
+            richTextBox3.Name = "richTextBox3";
+            richTextBox3.Size = new Size(53, 27);
+            richTextBox3.TabIndex = 8;
+            richTextBox3.Text = "Zdraví";
+            richTextBox3.TextChanged += richTextBox3_TextChanged;
+            // 
+            // richTextBox4
+            // 
+            richTextBox4.Location = new Point(149, 41);
+            richTextBox4.Name = "richTextBox4";
+            richTextBox4.Size = new Size(69, 27);
+            richTextBox4.TabIndex = 9;
+            richTextBox4.Text = "Jméno";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(402, 50);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 29);
+            button1.TabIndex = 10;
+            button1.Text = "Uložit";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // buttonDelete
+            // 
+            buttonDelete.Location = new Point(402, 85);
+            buttonDelete.Name = "buttonDelete";
+            buttonDelete.Size = new Size(127, 29);
+            buttonDelete.TabIndex = 11;
+            buttonDelete.Text = "Smazat";
+            buttonDelete.UseVisualStyleBackColor = true;
+            buttonDelete.Click += buttonDelete_Click;
+            // 
+            // richTextBoxDetails
+            // 
+            richTextBoxDetails.Enabled = false;
+            richTextBoxDetails.Location = new Point(9, 159);
+            richTextBoxDetails.Name = "richTextBoxDetails";
+            richTextBoxDetails.ReadOnly = true;
+            richTextBoxDetails.Size = new Size(413, 515);
+            richTextBoxDetails.TabIndex = 12;
+            richTextBoxDetails.Text = "";
+            richTextBoxDetails.TextChanged += richTextBoxDetails_TextChanged;
+            // 
+            // richTextBox5
+            // 
+            richTextBox5.Location = new Point(428, 137);
+            richTextBox5.Name = "richTextBox5";
+            richTextBox5.Size = new Size(53, 27);
+            richTextBox5.TabIndex = 13;
+            richTextBox5.Text = "Příběh";
+            // 
+            // richTextBoxLore
+            // 
+            richTextBoxLore.Location = new Point(428, 159);
+            richTextBoxLore.Name = "richTextBoxLore";
+            richTextBoxLore.Size = new Size(492, 515);
+            richTextBoxLore.TabIndex = 14;
+            richTextBoxLore.Text = "";
+            richTextBoxLore.TextChanged += richTextBoxLore_TextChanged;
+            // 
+            // buttonExport
+            // 
+            buttonExport.Location = new Point(-5, -4);
+            buttonExport.Name = "buttonExport";
+            buttonExport.Size = new Size(127, 29);
+            buttonExport.TabIndex = 15;
+            buttonExport.Text = "Export";
+            buttonExport.UseVisualStyleBackColor = true;
+            buttonExport.Click += buttonExport_Click;
+            // 
+            // buttonImport
+            // 
+            buttonImport.Location = new Point(128, -4);
+            buttonImport.Name = "buttonImport";
+            buttonImport.Size = new Size(127, 29);
+            buttonImport.TabIndex = 16;
+            buttonImport.Text = "Import";
+            buttonImport.UseVisualStyleBackColor = true;
+            buttonImport.Click += buttonImport_Click;
+            // 
+            // richTextBox6
+            // 
+            richTextBox6.Location = new Point(926, 12);
+            richTextBox6.Name = "richTextBox6";
+            richTextBox6.Size = new Size(85, 27);
+            richTextBox6.TabIndex = 17;
+            richTextBox6.Text = "Postavy";
+            richTextBox6.TextChanged += richTextBox6_TextChanged;
+            // 
+            // comboBoxAdditionalAttributes
+            // 
+            comboBoxAdditionalAttributes.FormattingEnabled = true;
+            comboBoxAdditionalAttributes.Location = new Point(562, 51);
+            comboBoxAdditionalAttributes.Name = "comboBoxAdditionalAttributes";
+            comboBoxAdditionalAttributes.Size = new Size(151, 28);
+            comboBoxAdditionalAttributes.TabIndex = 18;
+            comboBoxAdditionalAttributes.SelectedIndexChanged += comboBoxAdditionalAttributes_SelectedIndexChanged;
+            // 
+            // buttonAddAttribute
+            // 
+            buttonAddAttribute.Location = new Point(733, 51);
+            buttonAddAttribute.Name = "buttonAddAttribute";
+            buttonAddAttribute.Size = new Size(118, 29);
+            buttonAddAttribute.TabIndex = 19;
+            buttonAddAttribute.Text = "Přidat atribut";
+            buttonAddAttribute.UseVisualStyleBackColor = true;
+            buttonAddAttribute.Click += buttonAddAttribute_Click;
+            // 
+            // textBoxAttributeValue
+            // 
+            textBoxAttributeValue.Location = new Point(562, 87);
+            textBoxAttributeValue.Name = "textBoxAttributeValue";
+            textBoxAttributeValue.Size = new Size(151, 27);
+            textBoxAttributeValue.TabIndex = 20;
+            textBoxAttributeValue.TextChanged += textBoxAttributeValue_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(1151, 686);
+            Controls.Add(textBoxAttributeValue);
+            Controls.Add(buttonAddAttribute);
+            Controls.Add(comboBoxAdditionalAttributes);
+            Controls.Add(richTextBox6);
+            Controls.Add(buttonImport);
+            Controls.Add(buttonExport);
+            Controls.Add(richTextBoxLore);
+            Controls.Add(richTextBox5);
+            Controls.Add(richTextBoxDetails);
+            Controls.Add(buttonDelete);
+            Controls.Add(button1);
+            Controls.Add(richTextBox4);
+            Controls.Add(richTextBox3);
+            Controls.Add(richTextBox2);
+            Controls.Add(richTextBox1);
+            Controls.Add(numericUpDownHealth);
+            Controls.Add(numericUpDownStrength);
+            Controls.Add(numericUpDownAge);
+            Controls.Add(listBoxCharacters);
+            Controls.Add(textBoxName);
+            Controls.Add(buttonAddCharacter);
+            Name = "Form1";
+            Text = "RPG Architect";
+            Load += Form1_Load_1;
+            ((System.ComponentModel.ISupportInitialize)numericUpDownAge).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownStrength).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownHealth).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonAddCharacter;
+        private TextBox textBoxName;
+        private ListBox listBoxCharacters;
+        private NumericUpDown numericUpDownAge;
+        private NumericUpDown numericUpDownStrength;
+        private NumericUpDown numericUpDownHealth;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private RichTextBox richTextBox3;
+        private RichTextBox richTextBox4;
+        private Button button1;
+        private Button buttonDelete;
+        private RichTextBox richTextBoxDetails;
+        private RichTextBox richTextBox5;
+        private RichTextBox richTextBoxLore;
+        private Button buttonExport;
+        private Button buttonImport;
+        private RichTextBox richTextBox6;
+        private ComboBox comboBoxAdditionalAttributes;
+        private Button buttonAddAttribute;
+        private TextBox textBoxAttributeValue;
+    }
+}
---
+++ RPG_Architect/Form1.cs
@@ -0,0 +1,370 @@
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+
+
+namespace RPG_Architect
+{
+    public partial class Form1 : Form
+    {
+
+        private List<Character> characters = new List<Character>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBoxAdditionalAttributes.Items.Clear();
+            comboBoxAdditionalAttributes.Items.AddRange(new string[]
+            {
+        "Místo narození",
+        "Datum narození",
+        "Otec",
+        "Matka",
+        "Zbraò",
+        "Zvláštní schopnost",
+        "Povolání v minulosti",
+        "Osudový nepøítel"
+            });
+        }
+
+        public class Character
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public int Strength { get; set; }
+            public int Health { get; set; }
+            public string Lore { get; set; }
+
+
+
+            public Dictionary<string, string> AdditionalAttributes { get; set; } = new Dictionary<string, string>();
+
+
+
+            // Konstruktor 
+            public Character(string name, int age, int strength, int health, string lore)
+            {
+                Name = name;
+                Age = age;
+                Strength = strength;
+                Health = health;
+                Lore = lore;
+
+                AdditionalAttributes = new Dictionary<string, string>();
+            }
+
+            // Postava v seznamu
+            public override string ToString()
+            {
+                string shownChar = Name;
+                return shownChar;
+            }
+
+
+        }
+        private void UpdateCharacterListBox()
+        {
+            // Vymazání aktuálních položek v ListBoxu
+            listBoxCharacters.Items.Clear();
+
+            // Pøidání postav do ListBoxu
+            foreach (var character in characters)
+            {
+                listBoxCharacters.Items.Add(character);  // Pøidá celý objekt postavy
+            }
+
+            // Nastavení, co se bude zobrazovat v ListBoxu (zobrazí pouze jméno postavy)
+            listBoxCharacters.DisplayMember = "Name";
+        }
+        private void ExportCharactersToJson(string filePath)
+        {
+            
+
+            string jsonString = JsonConvert.SerializeObject(characters, Formatting.Indented);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        private void ImportCharactersFromJson(string filePath)
+        {
+            try
+            {
+                string jsonString = File.ReadAllText(filePath);
+
+                // Používáme System.Text.Json pro deserializaci
+                var importedCharacters = JsonConvert.DeserializeObject<List<Character>>(jsonString);
+
+                if (importedCharacters != null)
+                {
+                    characters.AddRange(importedCharacters);
+                    UpdateCharacterListBox();
+                    MessageBox.Show("Postavy byly úspìšnì importovány.", "Hotovo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Soubor neobsahuje žádná data.", "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Chyba pøi naèítání: " + ex.Message, "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+
+        // pøidání postavy
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBoxName.Text;
+            int age = (int)numericUpDownAge.Value;
+            int strength = (int)numericUpDownStrength.Value;
+            int health = (int)numericUpDownHealth.Value;
+            string lore = richTextBoxLore.Text;
+
+            // Vytvoøení postavy
+            Character newCharacter = new Character(name, age, strength, health, lore);
+
+            // Pøidání postavy do seznamu
+            characters.Add(newCharacter);
+
+            // Pøidání postavy do ListBoxu
+            listBoxCharacters.Items.Add(newCharacter);
+
+            // Vymazání TextBoxù po pøidání postavy
+            textBoxName.Clear();
+            numericUpDownAge.Value = 0;
+            numericUpDownStrength.Value = 0;
+            numericUpDownHealth.Value = 0;
+            richTextBoxDetails.Clear();
+            richTextBoxLore.Clear();
+        }
+
+
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = listBoxCharacters.SelectedIndex;
+
+            if (selectedIndex >= 0 && selectedIndex < characters.Count)
+            {
+                Character selectedChar = characters[selectedIndex];
+
+                // Zobrazit info v editaèních polích
+                textBoxName.Text = selectedChar.Name;
+                numericUpDownAge.Value = selectedChar.Age;
+                numericUpDownStrength.Value = selectedChar.Strength;
+                numericUpDownHealth.Value = selectedChar.Health;
+                richTextBoxDetails.Text = selectedChar.Lore;
+                richTextBoxLore.Text = selectedChar.Lore;
+
+                // Zobrazit také v textovém poli
+                richTextBoxDetails.Text = $"Jméno: {selectedChar.Name}\n" +
+                                                   $"Vìk: {selectedChar.Age}\n" +
+                                                   $"Síla: {selectedChar.Strength}\n" +
+                                                   $"Zdraví: {selectedChar.Health}\n" +
+                                                   $"Pøíbìh: {selectedChar.Lore}";
+            }
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            string currentVersion = "0.1";
+            this.Text = "RPG Architect v" + currentVersion;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+
+            int selectedIndex = listBoxCharacters.SelectedIndex;
+            if (selectedIndex >= 0)
+            {
+                string selectedName = listBoxCharacters.SelectedItem.ToString();
+
+                // Zobrazíme potvrzovací okno
+                DialogResult result = MessageBox.Show(
+                    $"Opravdu chceš smazat postavu „{selectedName}“?",
+                    "Potvrzení smazání",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning
+                );
+
+                // Pokud uživatel klikne na "Ano"
+                if (result == DialogResult.Yes)
+                {
+                    listBoxCharacters.Items.RemoveAt(selectedIndex);
+                    characters.RemoveAt(selectedIndex); // Pokud máš List<Character> characters
+                    textBoxName.Clear();
+                    numericUpDownAge.Value = 0;
+                    numericUpDownStrength.Value = 0;
+                    numericUpDownHealth.Value = 0;
+                    richTextBoxDetails.Clear();
+                    richTextBoxLore.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nejprve prosím vyber postavu, kterou chceš smazat.");
+            }
+        }
+
+        private void richTextBoxDetails_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int selectedIndex = listBoxCharacters.SelectedIndex;
+
+            if (selectedIndex >= 0 && selectedIndex < characters.Count)
+            {
+                // Získání postavy
+                Character selectedChar = characters[selectedIndex];
+
+                // Aktualizace dat
+                selectedChar.Name = textBoxName.Text;
+                selectedChar.Age = (int)numericUpDownAge.Value;
+                selectedChar.Strength = (int)numericUpDownStrength.Value;
+                selectedChar.Health = (int)numericUpDownHealth.Value;
+                selectedChar.Lore = richTextBoxLore.Text;
+
+                // Aktualizace
+                listBoxCharacters.Items[selectedIndex] = selectedChar.Name;
+
+                richTextBoxDetails.Text = $"Jméno: {selectedChar.Name}\n" +
+                                                   $"Vìk: {selectedChar.Age}\n" +
+                                                   $"Síla: {selectedChar.Strength}\n" +
+                                                   $"Zdraví: {selectedChar.Health}\n" +
+                                                   $"Pøíbìh: {selectedChar.Lore}";
+
+                MessageBox.Show("Postava byla úspìšnì upravena.", "Hotovo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBoxName.Clear();
+                numericUpDownAge.Value = 0;
+                numericUpDownStrength.Value = 0;
+                numericUpDownHealth.Value = 0;
+                richTextBoxDetails.Clear();
+                richTextBoxLore.Clear();
+            }
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBoxLore_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonExport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JSON files (*.json)|*.json";
+            saveFileDialog.Title = "Uložit postavy jako JSON";
+
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ExportCharactersToJson(saveFileDialog.FileName);
+                MessageBox.Show("Postavy byly úspìšnì exportovány.", "Hotovo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void buttonImport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JSON files (*.json)|*.json";
+            openFileDialog.Title = "Naèíst postavy ze souboru";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ImportCharactersFromJson(openFileDialog.FileName);
+            }
+        }
+
+        private void richTextBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxAdditionalAttributes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBoxAdditionalAttributes.Items.Add("Místo narození");
+            comboBoxAdditionalAttributes.Items.Add("Rodièe");
+            comboBoxAdditionalAttributes.Items.Add("Povolání");
+            comboBoxAdditionalAttributes.Items.Add("Zvláštní schopnosti");
+
+
+        }
+
+        private void buttonAddAttribute_Click(object sender, EventArgs e)
+        {
+            // Získání vybrané postavy z listboxu
+            var selectedCharacter = (Character)listBoxCharacters.SelectedItem;
+
+            // Pokud je postava vybraná
+            if (selectedCharacter != null)
+            {
+                // Získání vybraného atributu a jeho hodnoty
+                string attributeName = comboBoxAdditionalAttributes.SelectedItem?.ToString();
+                string attributeValue = textBoxAttributeValue.Text;
+
+                // Pokud jsou oba atributy zadány
+                if (!string.IsNullOrEmpty(attributeName) && !string.IsNullOrEmpty(attributeValue))
+                {
+                    // Pøidání do AdditionalAttributes
+                    selectedCharacter.AdditionalAttributes[attributeName] = attributeValue;
+
+                    // Aktualizování UI (napø. zobrazit pøidaný atribut)
+                    UpdateCharacterListBox();
+                }
+                else
+                {
+                    MessageBox.Show("Vyberte atribut a zadejte jeho hodnotu.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vyberte postavu.");
+            }
+        }
+
+        private void textBoxAttributeValue_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
